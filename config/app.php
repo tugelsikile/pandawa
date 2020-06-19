b2a174ec825a2e7e8060b37b8881600294737cee
tugelsikile <52416894+tugelsikile@users.noreply.github.com>
2020-06-19 09:09:14 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\ValidatorServiceProvider::class,
-        Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider::class,
+        '\Rap2hpoutre\LaravelLogViewer\LaravelLogViewerServiceProvider',
 
     ],
 
