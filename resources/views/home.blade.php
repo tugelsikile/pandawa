4f0dfbfbf7b4bbefe149872b3550bcaf7c8257ba
tugelsikile <52416894+tugelsikile@users.noreply.github.com>
2020-07-08 23:51:05 +0700

    bs4 icons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,16 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+        <div class="card">
+            <div class="card-body">asda</div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+        <div class="card-body">
+            Selamat Datang {{ auth()->user()->name }}
         </div>
     </div>
 </div>
